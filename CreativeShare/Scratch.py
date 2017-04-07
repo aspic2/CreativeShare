686fb9be7593228eae0bc73cca91eb6701d858dc
Thomp <thomp@protonmail.com>
2017-04-07 15:27:57 -0500

    creating logic for getCreativeSizes method

--- CreativeShare/Scratch.py
+++ CreativeShare/Scratch.py
@@ -1,60 +1,41 @@
-#!/usr/bin/python
-#
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""This code example creates new line item creative associations (LICAs) for an
-existing line item and a set of creative ids.
-To determine which LICAs exist, run get_all_licas.py.
-"""
-
-
 # Import appropriate modules from the client library.
 from googleads import dfp
-import googleads
-import logging
-from sortedcontainers import SortedDict
-# Set the line item ID and creative IDs to associate.
-LINE_ITEM_ID = '307238069'
-CREATIVE_IDS = ['117644870909']
 
 
-def main(client, line_item_id, creative_ids):
-    logging.basicConfig(level=logging.INFO, format=googleads.util.LOGGER_FORMAT)
-    logging.getLogger('suds.transport').setLevel(logging.DEBUG)
+def main(client):
     # Initialize appropriate service.
-    lica_service = client.GetService(
-        'LineItemCreativeAssociationService', version='v201702')
+    line_item_service = client.GetService('LineItemService', version='v201702')
+    query = 'WHERE id = :id'
+    values = [
+      {'key': 'id',
+       'value': {
+           'xsi_type': 'NumberValue',
+           'value': '291270989'
+       }},
+    ]
+    # Create a statement to select line items.
+    statement = dfp.FilterStatement(query, values)
 
-    licas = []
-    for creative_id in creative_ids:
-        licas.append({'creativeId': creative_id,
-                  'lineItemId': line_item_id})
+    # Retrieve a small amount of line items at a time, paging
+    # through until all line items have been retrieved.
+    while True:
+        response = line_item_service.getLineItemsByStatement(
+        statement.ToStatement())
+        if 'results' in response:
+            for line_item in response['results']:
+                creativePlaceholders = line_item['creativePlaceholders']
+                size = creativePlaceholders[0]
+            # Print out some information for each line item.
+                print('Line Item ID "%d"\nSize = %s\n' %
+                (line_item['id'], size))
+            statement.offset += dfp.SUGGESTED_PAGE_LIMIT
+        else:
+            break
 
-    # Create the LICAs remotely.
-    licas = lica_service.createLineItemCreativeAssociations(licas)
+    print('\nNumber of results found: %s' % response['totalResultSetSize'])
 
-    # Display results.
-    if licas:
-        for lica in licas:
-            print ('LICA with line item id \'%s\', creative id \'%s\', and '
-            'status \'%s\' was created.' %
-            (lica['lineItemId'], lica['creativeId'], lica['status']))
-    else:
-        print('No LICAs created.')
 
 if __name__ == '__main__':
-    # Initialize client object.
-    dfp_client = dfp.DfpClient.LoadFromStorage()
-    main(dfp_client, LINE_ITEM_ID, CREATIVE_IDS)
+  # Initialize client object.
+  dfp_client = dfp.DfpClient.LoadFromStorage()
+  main(dfp_client)
